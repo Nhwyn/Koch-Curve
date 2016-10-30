480f8715be7b88c57f1f9e2cf264b64eac8761eb
Nhwyn <alchemistdn93@gmail.com>
2016-10-29 18:15:52 -0700

    Update Koch-Curve.py

--- Koch-Curve.py
+++ Koch-Curve.py
@@ -3,7 +3,9 @@
 Created on Thu Sep  8 21:15:17 2016
 
 @author: Dom
+
 """
+
 
 from graphics import *
 from math import *
@@ -11,6 +13,8 @@ win = GraphWin("test", 1080, 720)
 win.setCoords(-100,-100,100,100)
 win.setBackground('white')
 
+"""Recursive function that takes in a turtle object, an integer for the length of line being curved, and a degree 
+for the amount of times the line is divided. At degree two there will be 4 sides at 12 there will be 4096"""
 def C(turtle, length, degree):
     if degree == 0:
         turtle.draw(length)
@@ -24,6 +28,8 @@ def C(turtle, length, degree):
         turtle.turn(45)
       
         
+"""Turtle class takes in a starting position (coordinate on a cartesian plane), a direction (degree starting angle of line),
+and win is defined above"""
 class Turtle:
     def __init__(self, location, direction,win):
         self.location = location
